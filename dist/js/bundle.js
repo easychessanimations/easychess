aa888e9f26e62a8cc398ef456aa9279cb9bb2e0c
easychessanimations <easychessanimations@gmail.com>
2020-02-17 00:22:10 +0100

    Bundle before versioning

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,36 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 },{}],2:[function(require,module,exports){
-/* eslint-env browser */
-module.exports = typeof self == 'object' ? self.FormData : window.FormData;
-
-},{}],3:[function(require,module,exports){
-(function (global){
-"use strict";
-
-// ref: https://github.com/tc39/proposal-global
-var getGlobal = function () {
-	// the only reliable means to get the global object is
-	// `Function('return this')()`
-	// However, this causes CSP violations in Chrome apps.
-	if (typeof self !== 'undefined') { return self; }
-	if (typeof window !== 'undefined') { return window; }
-	if (typeof global !== 'undefined') { return global; }
-	throw new Error('unable to locate global object');
-}
-
-var global = getGlobal();
-
-module.exports = exports = global.fetch;
-
-// Needed for TypeScript and Webpack.
-exports.default = global.fetch.bind(global);
-
-exports.Headers = global.Headers;
-exports.Request = global.Request;
-exports.Response = global.Response;
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],4:[function(require,module,exports){
 (function (process){
 // .dirname, .basename, and .extname methods are extracted from Node.js v8.11.1,
 // backported and transplited with Babel, with backwards-compat fixes
@@ -336,7 +306,7 @@ var substr = 'ab'.substr(-1) === 'b'
 ;
 
 }).call(this,require('_process'))
-},{"_process":5}],5:[function(require,module,exports){
+},{"_process":3}],3:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
@@ -522,6 +492,36 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
+},{}],4:[function(require,module,exports){
+/* eslint-env browser */
+module.exports = typeof self == 'object' ? self.FormData : window.FormData;
+
+},{}],5:[function(require,module,exports){
+(function (global){
+"use strict";
+
+// ref: https://github.com/tc39/proposal-global
+var getGlobal = function () {
+	// the only reliable means to get the global object is
+	// `Function('return this')()`
+	// However, this causes CSP violations in Chrome apps.
+	if (typeof self !== 'undefined') { return self; }
+	if (typeof window !== 'undefined') { return window; }
+	if (typeof global !== 'undefined') { return global; }
+	throw new Error('unable to locate global object');
+}
+
+var global = getGlobal();
+
+module.exports = exports = global.fetch;
+
+// Needed for TypeScript and Webpack.
+exports.default = global.fetch.bind(global);
+
+exports.Headers = global.Headers;
+exports.Request = global.Request;
+exports.Response = global.Response;
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],6:[function(require,module,exports){
 const utils = require('./utils')
 
@@ -2571,7 +2571,7 @@ console.log("created", b)
 
 b.stream()
 }).call(this,require('_process'))
-},{"./lichessbot":9,"./utils":10,"_process":5}],8:[function(require,module,exports){
+},{"./lichessbot":9,"./utils":10,"_process":3}],8:[function(require,module,exports){
 const utils = require('./utils')
 const FormData = require('form-data')
 
@@ -3117,7 +3117,7 @@ module.exports = {
 }
 */
 
-},{"./utils":10,"form-data":2}],9:[function(require,module,exports){
+},{"./utils":10,"form-data":4}],9:[function(require,module,exports){
 (function (process,__dirname){
 const lichess = require('./lichess')
 const utils = require('./utils')
@@ -3495,7 +3495,7 @@ function LichessBot(props){return new LichessBot_(props)}
 module.exports.LichessBot = LichessBot
 
 }).call(this,require('_process'),"/resources/client/nodejs")
-},{"./chessboard":6,"./lichess":8,"./utils":10,"_process":5,"child_process":1,"path":4}],10:[function(require,module,exports){
+},{"./chessboard":6,"./lichess":8,"./utils":10,"_process":3,"child_process":1,"path":2}],10:[function(require,module,exports){
 const fetch = require('node-fetch')
 
 const P = p => new Promise(p)
@@ -3991,4 +3991,4 @@ module.exports.WEEK = WEEK
 module.exports.MONTH = MONTH
 module.exports.YEAR = YEAR
 
-},{"node-fetch":3}]},{},[7]);
+},{"node-fetch":5}]},{},[7]);
--- s/cv.bat
+++ s/cv.bat
@@ -1,1 +1,3 @@
+browserify resources\client\nodejs\index.js -o dist\js\bundle.js
+
 node resources/utils/createversioninfo.js
