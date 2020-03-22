772a2d43baadb9b3498ecb5785dcb089dbdf3fd0
easychessanimations <easychessanimations@gmail.com>
2020-03-22 22:26:01 +0100

    Bulk delete message discord bot

--- resources/server/discordbot.js
+++ resources/server/discordbot.js
@@ -5,9 +5,34 @@ client.on('ready', _ => {
     console.log(`discord bot logged in as ${client.user.tag}!`)
 })
 
+async function bulkDelete(channel, limit){
+    console.log("bulk delete", channel.id, limit)
+    const fetched = await channel.messages.fetch({limit: limit + 1})    
+    fetched.forEach(message => {
+        message.delete()
+    })
+}
+
 client.on('message', msg => {
-    if (msg.content === 'ping'){
+    let content = msg.content
+
+    if(content === 'ping'){
         msg.reply('pong')
+    }
+
+    let m
+
+    if(msg.member.guild.me.hasPermission("ADMINISTRATOR")){
+        ////////////////////////////////////////////////
+        // ADMIN
+
+        console.log("admin message", content)
+
+        if(m = content.match(/^!del (\d+)/)){
+            bulkDelete(msg.channel, parseInt(m[1]))
+        }
+
+        ////////////////////////////////////////////////
     }
 })
 
